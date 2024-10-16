96ac59c4c74ef403623e7d222da3098fb26c9c93
Vivek <vc1262461@gmail.com>
2024-10-16 22:13:40 +0530

    Minor Education AI Bug Fixed

--- apps/payroll/app/components/employees/filter-list.tsx
+++ apps/payroll/app/components/employees/filter-list.tsx
@@ -1,7 +1,7 @@
 import type { EmployeeFilters } from "@canny_ecosystem/supabase/queries";
 import { Button } from "@canny_ecosystem/ui/button";
 import { Icon } from "@canny_ecosystem/ui/icon";
-import { formatDate, formatDateRange } from "@canny_ecosystem/utils";
+import { formatDate, formatDateRange, replaceDash } from "@canny_ecosystem/utils";
 import { useSearchParams } from "@remix-run/react";
 
 export type EmployeeFilterList = EmployeeFilters & {
@@ -31,7 +31,7 @@ export function FilterList({ filterList }: Props) {
         return value;
 
       case "education":
-        return value;
+        return replaceDash(value);
 
       case "status":
         return value;
@@ -66,7 +66,7 @@ export function FilterList({ filterList }: Props) {
                 >
                   <Icon
                     name="cross"
-                    className="scale-0 group-hover:scale-100 transition-all w-0 group-hover:w-4"
+                    className="scale-0 group-hover:scale-100 transition-all w-0 capitalize group-hover:w-4"
                   />
                   <span>
                     {renderFilter({
