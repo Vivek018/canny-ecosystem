4338ea53a77565b7e5303e1493fb14c73f7e62dd
sahilanand30 <sahilhanand@gmail.com>
2025-03-04 10:42:58 +0530

    fixed spacing of document cards

--- apps/management/app/routes/_protected+/employees+/$employeeId+/documents+/_index.tsx
+++ apps/management/app/routes/_protected+/employees+/$employeeId+/documents+/_index.tsx
@@ -1,25 +1,21 @@
-import { Card, CardTitle } from "@canny_ecosystem/ui/card";
 import { buttonVariants } from "@canny_ecosystem/ui/button";
-import { createRole, hasPermission, replaceUnderscore, updateRole } from "@canny_ecosystem/utils";
+import { createRole, hasPermission } from "@canny_ecosystem/utils";
 import { defer, type LoaderFunctionArgs } from "@remix-run/node";
 import { getSupabaseWithHeaders } from "@canny_ecosystem/supabase/server";
 import { getEmployeeDocuments } from "@canny_ecosystem/supabase/media";
-import { Await, type ClientLoaderFunctionArgs, Link, Outlet, useLoaderData, useNavigate, useParams } from "@remix-run/react";
+import { Await, type ClientLoaderFunctionArgs, Link, Outlet, useLoaderData, useParams } from "@remix-run/react";
 import { Suspense, useEffect } from "react";
 import { clearExactCacheEntry, clientCaching } from "@/utils/cache";
 import { cacheKeyPrefix } from "@/constant";
 import { ErrorBoundary } from "@/components/error-boundary";
-import { toast, useToast } from "@canny_ecosystem/ui/use-toast";
-import { Icon } from "@canny_ecosystem/ui/icon";
+import { useToast } from "@canny_ecosystem/ui/use-toast";
 import { useUser } from "@/utils/user";
 import { useIsDocument } from "@canny_ecosystem/utils/hooks/is-document";
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@canny_ecosystem/ui/command";
 import { cn } from "@canny_ecosystem/ui/utils/cn";
 import { attribute } from "@canny_ecosystem/utils/constant";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@canny_ecosystem/ui/dropdown-menu";
-import { DeleteEmployeeDocument } from "@/components/employees/documents/delete-employee-document";
-import { Avatar, AvatarFallback, AvatarImage } from "@canny_ecosystem/ui/avatar";
 import type { EmployeeDocumentsDatabaseRow } from "@canny_ecosystem/supabase/types";
+import DocumentCard from "@/components/employees/documents/document-card";
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
     const employeeId = params.employeeId ?? "";
@@ -140,7 +136,7 @@ export function DocumentsWrapper({ data, error }: {
 
     return (
         <CommandGroup>
-            <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-0">
+            <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-10">
                 {
                     data.map((document) => {
                         return <CommandItem
@@ -154,114 +150,5 @@ export function DocumentsWrapper({ data, error }: {
                 }
             </div>
         </CommandGroup>
-    );
-}
-
-// document card
-export function DocumentCard({ documentData }: {
-    documentData: {
-        name: string;
-        url: string;
-    }
-}) {
-    const { role } = useUser();
-    const { employeeId } = useParams();
-    const navigate = useNavigate();
-
-    const handleDownload = async (url: string, filename: string) => {
-        try {
-            const response = await fetch(url);
-            const blob = await response.blob();
-            const blobUrl = window.URL.createObjectURL(blob);
-
-            const link = document.createElement("a");
-            link.href = blobUrl;
-            link.download = filename;
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-
-            // Free memory after download
-            window.URL.revokeObjectURL(blobUrl);
-
-            toast({
-                title: "Success",
-                description: `${filename} downloaded successfully`,
-                variant: "success",
-            });
-        } catch (error) {
-            console.error("Download failed", error);
-            toast({
-                title: "Error",
-                description: "Download failed, please try again",
-                variant: "destructive",
-            });
-        }
-    };
-
-    const handleEdit = (documentName: string) => {
-        navigate(`/employees/${employeeId}/documents/edit-document?documentName=${documentName}`);
-    }
-
-    return (
-        <Card
-            key={documentData.name}
-            className="w-full max-w-[200px] select-text cursor-pointer flex flex-col overflow-hidden rounded-lg shadow-md transition-transform duration-300"
-        >
-            <div
-                className="flex items-center justify-center bg-muted/30 hover:bg-muted/50 h-48 w-full"
-            >
-                <Avatar className="h-full w-full rounded-md">
-                    <>
-                        <AvatarImage src={documentData.url} alt={documentData.name} className="h-full w-full object-cover" />
-                        <AvatarFallback className="h-full w-full rounded-md bg-secondary/70 flex items-center justify-center capitalize text-sm font-medium">
-                            {replaceUnderscore(documentData.name)}
-                        </AvatarFallback>
-                    </>
-                </Avatar>
-            </div>
-
-            <div className="px-4 py-3 flex justify-between items-center border-t">
-                {/* Card Title */}
-                <CardTitle
-                    className="text-sm tracking-wide hover:text-primary capitalize truncate"
-                    onClick={() => window.open(documentData.url, '_blank')}
-                >
-                    {replaceUnderscore(documentData.name)}
-                </CardTitle>
-
-                {/* Dropdown Menu */}
-                <DropdownMenu>
-                    <DropdownMenuTrigger className="p-1 rounded-full hover:bg-secondary grid place-items-center">
-                        <Icon name="dots-vertical" size="xs" />
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent sideOffset={8} align="end">
-                        <DropdownMenuItem
-                            onClick={() => documentData.url && handleDownload(documentData.url, `${documentData.name}_${employeeId}.pdf`)}
-                            className={cn(
-                                "hidden",
-                                hasPermission(role, `${updateRole}:${attribute.employeeDocuments}`) &&
-                                "flex",
-                            )}
-                        >
-                            Download
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem
-                            onClick={() => handleEdit(documentData.name)}
-                            className={cn(
-                                "hidden",
-                                hasPermission(role, `${updateRole}:${attribute.employeeDocuments}`) &&
-                                "flex",
-                            )}
-                        >
-                            Edit document
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DeleteEmployeeDocument employeeId={employeeId as string} documentName={documentData.name} />
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </div>
-        </Card>
     );
 }
