0c9873ed86c85d8fe8e7641fffb4c343aeb084a1
Vivek <vc1262461@gmail.com>
2025-05-12 16:57:46 +0530

    few more fixes

--- packages/supabase/src/constant.ts
+++ packages/supabase/src/constant.ts
@@ -1,5 +1,5 @@
 export const SINGLE_QUERY_LIMIT = 1;
-export const LAZY_LOADING_LIMIT = 100;
+export const LAZY_LOADING_LIMIT = 60;
 export const HARD_QUERY_LIMIT = 100;
 export const MID_QUERY_LIMIT = 1000;
 export const SOFT_QUERY_LIMIT = 10000;
