7ab1035db9cdfb5f5a5d5c0f53585375884a636e
Vivek <vc1262461@gmail.com>
2025-05-27 15:04:50 +0530

    Change in Employee App

--- apps/employee/app/routes/_protected+/employees+/$employeeId+/payments+/_index.tsx
+++ apps/employee/app/routes/_protected+/employees+/$employeeId+/payments+/_index.tsx
@@ -1,14 +1,10 @@
 import { ExitsCard } from "@/components/employees/exits/exits-card";
-import { LinkTemplateCard } from "@/components/employees/link-template/link-template-card";
 import { ErrorBoundary } from "@/components/error-boundary";
 import { LoadingSpinner } from "@/components/loading-spinner";
-import { PaymentTemplateComponentsCard } from "@/components/payment-templates/payment-template-components-card";
 import { cacheKeyPrefix } from "@/constant";
 import { clearExactCacheEntry, clientCaching } from "@/utils/cache";
 import {
   getExitByEmployeeId,
-  getPaymentTemplateAssignmentByEmployeeId,
-  getPaymentTemplateComponentsByTemplateId,
 } from "@canny_ecosystem/supabase/queries";
 import { getSupabaseWithHeaders } from "@canny_ecosystem/supabase/server";
 import { defer, type LoaderFunctionArgs } from "@remix-run/node";
@@ -25,34 +21,34 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   const employeeId = params.employeeId as string;
 
   try {
-    const paymentTemplateAssignmentPromise =
-      getPaymentTemplateAssignmentByEmployeeId({
-        supabase,
-        employeeId,
-      });
-    const paymentTemplateComponentsPromise =
-      paymentTemplateAssignmentPromise.then((assignmentResult) => {
-        if (assignmentResult.data?.template_id) {
-          return getPaymentTemplateComponentsByTemplateId({
-            supabase,
-            templateId: assignmentResult.data.template_id,
-          });
-        }
-        return { data: null, error: null };
-      });
+    // const paymentTemplateAssignmentPromise =
+    //   getPaymentTemplateAssignmentByEmployeeId({
+    //     supabase,
+    //     employeeId,
+    //   });
+    // const paymentTemplateComponentsPromise =
+    //   paymentTemplateAssignmentPromise.then((assignmentResult) => {
+    //     if (assignmentResult.data?.template_id) {
+    //       return getPaymentTemplateComponentsByTemplateId({
+    //         supabase,
+    //         templateId: assignmentResult.data.template_id,
+    //       });
+    //     }
+    //     return { data: null, error: null };
+    //   });
     const exitsPromise = getExitByEmployeeId({ supabase, employeeId });
 
     return defer({
-      paymentTemplateAssignmentPromise,
-      paymentTemplateComponentsPromise,
+      // paymentTemplateAssignmentPromise,
+      // paymentTemplateComponentsPromise,
       exitsPromise,
       error: null,
     });
   } catch (error) {
     return defer({
       error,
-      paymentTemplateAssignmentPromise: null,
-      paymentTemplateComponentsPromise: null,
+      // paymentTemplateAssignmentPromise: null,
+      // paymentTemplateComponentsPromise: null,
       exitsPromise: null,
     });
   }
@@ -69,8 +65,8 @@ clientLoader.hydrate = true;
 
 export default function Payments() {
   const {
-    paymentTemplateAssignmentPromise,
-    paymentTemplateComponentsPromise,
+    // paymentTemplateAssignmentPromise,
+    // paymentTemplateComponentsPromise,
     exitsPromise,
     error,
   } = useLoaderData<typeof loader>();
@@ -83,7 +79,7 @@ export default function Payments() {
 
   return (
     <div className="w-full py-4 flex flex-col gap-8">
-      <Suspense fallback={<LoadingSpinner className="h-1/4" />}>
+      {/* <Suspense fallback={<LoadingSpinner className="h-1/4" />}>
         <Await resolve={paymentTemplateAssignmentPromise}>
           {(resolvedAssignment) => {
             if (!resolvedAssignment) {
@@ -112,7 +108,7 @@ export default function Payments() {
             );
           }}
         </Await>
-      </Suspense>
+      </Suspense> */}
 
       <Suspense fallback={<LoadingSpinner className="h-1/4" />}>
         <Await resolve={exitsPromise}>
